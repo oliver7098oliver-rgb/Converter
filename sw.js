1e484d02209e79f68a9331426fa6f903d4b9dc02
oliver7098oliver-rgb <oliver7098oliver@gmail.com>
2026-04-06 18:09:05 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+<!-- ===================== sw.js ===================== -->
+self.addEventListener('install', e => {
+  console.log('SW instalado');
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
